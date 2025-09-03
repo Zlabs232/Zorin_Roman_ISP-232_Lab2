69eb936d9949e1882d92369d05f4b230a7a42865
Роман Зорин <sgena170315@gmail.com>
2025-09-03 16:20:47 +0300

    Различие делений с заданным типом данных и нет

--- Zorin_Roman_ISP-232_Lab2/Program.cs
+++ Zorin_Roman_ISP-232_Lab2/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            double a = 10;
+            double b = 3;
+            double c = a / b;
+            Console.WriteLine($"Переменная c: {c}");
+            double x = 10 / 3;
+            Console.WriteLine($"Переменная х: {x}");
+
         }
     }
 }
