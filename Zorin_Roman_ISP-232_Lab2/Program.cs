d11ed43ad53c94312f759955f360f7bc579cd4f2
Роман Зорин <sgena170315@gmail.com>
2025-09-03 18:57:37 +0300

    🏁 Задание 12. Только положительные

--- Zorin_Roman_ISP-232_Lab2/Program.cs
+++ Zorin_Roman_ISP-232_Lab2/Program.cs
@@ -4,14 +4,16 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Выберите класс (воин, маг, разбойник, паладин, варвар, охотник, друид, жрец): ");
-        string CC = Console.ReadLine();
+        Console.Write("Введите первое число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine(CC switch
-        {
-            "воин" or "паладин" or "варвар" or "разбойник" => "ближний боец",
-            "маг" or "охотник" or "жрец" or "друид" => "дальнобойщик",
-            _ => "неизвестный класс"
-        });
+        Console.Write("Введите второе число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите третье число: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = (num1 > 0 ? num1 : 0) + (num2 > 0 ? num2 : 0) + (num3 > 0 ? num3 : 0);
+        Console.WriteLine(sum);
     }
 }
