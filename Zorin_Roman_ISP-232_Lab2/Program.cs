14445f953bdc8a5e9cd78b93f8aebaa34f2537ac
Роман Зорин <sgena170315@gmail.com>
2025-09-03 19:10:05 +0300

    Задание 13. Путешествие в Темный Лабиринт

--- Zorin_Roman_ISP-232_Lab2/Program.cs
+++ Zorin_Roman_ISP-232_Lab2/Program.cs
@@ -4,16 +4,23 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Введите первое число: ");
-        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Вы — отважный искатель приключений, ваша задача - найти сокровища легендарного Dungeon Master'a. Вам требуется пройти через несколько комнат. Вы стоите перед выбором. Пройти в дверь с Драконом(1) или пойти по тёмному коридору(2). Что же вы выберете?");
+        int choice = int.Parse(Console.ReadLine());
+        string res = choice == 1 ? DragonPath() : DarkCPath();
+        Console.WriteLine(res);
+    }
+    static string DragonPath()
+    {
+        Console.WriteLine("Вам нужно ответить на загадку дракона! \nДракон: 'Кто не дышит, но живет; хоть не нужно — много пьет; и в жизни, и в смерти тело как лед.'");
+        Console.WriteLine("Ваш ответ: ");
+        string answer = Console.ReadLine().ToLower();
+        return answer == "рыба" ? "Дракон: 'Красавчик! Дуй отсюда'" : "Дракон: 'Мдаа, ну полезай мне в рот, ща тебя сожру'";
+    }
+    static string DarkCPath()
+    {
+        Console.WriteLine("Вы в темной комнате с двумя дверями. Выберите дверь 1 или 2:");
+        int doorChoice = int.Parse(Console.ReadLine());
 
-        Console.Write("Введите второе число: ");
-        int num2 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Введите третье число: ");
-        int num3 = Convert.ToInt32(Console.ReadLine());
-
-        int sum = (num1 > 0 ? num1 : 0) + (num2 > 0 ? num2 : 0) + (num3 > 0 ? num3 : 0);
-        Console.WriteLine(sum);
+        return doorChoice == 1 ? "Молоток! Ты нашел сокровища Dungeon Master'a" : "Ты сдох от ядовитых шипов, позоооор. Игра окончена.";
     }
 }
